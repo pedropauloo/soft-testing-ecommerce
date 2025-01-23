54d51c46a33a7049056f48f593b5ddbd480afec9
José Ben Hur Nascimento <90663589+Benhurds12@users.noreply.github.com>
2025-01-23 02:20:10 -0300

    Update EstoqueSimulado.java

--- eCommerce-v20242/src/main/java/ecommerce/external/fake/EstoqueSimulado.java
+++ eCommerce-v20242/src/main/java/ecommerce/external/fake/EstoqueSimulado.java
@@ -1,6 +1,9 @@
 package ecommerce.external.fake;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
@@ -10,4 +13,42 @@ import ecommerce.external.IEstoqueExternal;
 
 @Service
 public class EstoqueSimulado implements IEstoqueExternal {
+
+    private final Map<Long, Integer> estoque;
+
+     public EstoqueSimulado() {
+        estoque = new HashMap<>();
+        estoque.put(1L, 10); 
+        estoque.put(2L, 5);  
+        estoque.put(3L, 0);  
+    }
+    @Override
+    public EstoqueBaixaDTO darBaixa(List<Long> produtosIds, List<Long> produtosQuantidades) {
+        for (int i = 0; i < produtosIds.size(); i++) {
+            Long produtoId = produtosIds.get(i);
+            Integer quantidade = produtosQuantidades.get(i).intValue();
+
+            if (estoque.containsKey(produtoId) && estoque.get(produtoId) >= quantidade) {
+                estoque.put(produtoId, estoque.get(produtoId) - quantidade); 
+            } else {
+                return new EstoqueBaixaDTO(false); 
+            }
+        }
+        return new EstoqueBaixaDTO(true);
+    }
+
+    @Override
+    public DisponibilidadeDTO verificarDisponibilidade(List<Long> produtosIds, List<Long> produtosQuantidades) {
+        List<Long> indisponiveis = new ArrayList<>();
+
+        for (int i = 0; i < produtosIds.size(); i++) {
+            Long produtoId = produtosIds.get(i);
+            Integer quantidade = produtosQuantidades.get(i).intValue();
+
+            if (!estoque.containsKey(produtoId) || estoque.get(produtoId) < quantidade) {
+                indisponiveis.add(produtoId);   
+            }
+        }
+        return new DisponibilidadeDTO(indisponiveis.isEmpty(), indisponiveis);
+    }
 }
