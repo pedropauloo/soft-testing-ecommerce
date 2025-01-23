26fe6d93c3d6481e0e6bb87155e428f1622e263b
José Ben Hur Nascimento <90663589+Benhurds12@users.noreply.github.com>
2025-01-23 02:20:25 -0300

    Update PagamentoSimulado.java

--- eCommerce-v20242/src/main/java/ecommerce/external/fake/PagamentoSimulado.java
+++ eCommerce-v20242/src/main/java/ecommerce/external/fake/PagamentoSimulado.java
@@ -1,5 +1,8 @@
 package ecommerce.external.fake;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
 import ecommerce.dto.PagamentoDTO;
@@ -7,4 +10,31 @@ import ecommerce.external.IPagamentoExternal;
 
 @Service
 public class PagamentoSimulado implements IPagamentoExternal{
+    
+    private Map<Long, PagamentoDTO> pagamentos;
+    private long proximoId = 1; 
+
+    public PagamentoSimulado() {
+        pagamentos = new HashMap<>(); 
+    }
+    @Override
+    public PagamentoDTO autorizarPagamento(Long clienteId, Double custoTotal) {
+        if (custoTotal > 0) {
+            PagamentoDTO pagamento = new PagamentoDTO(true, proximoId); 
+            pagamentos.put(proximoId, pagamento); 
+            proximoId++; 
+            return pagamento; 
+        } else {
+            return new PagamentoDTO(false, null);
+        }
+    }
+    @Override
+    public void cancelarPagamento(Long clienteId, Long pagamentoTransacaoId) {
+        if (pagamentos.containsKey(pagamentoTransacaoId)) {
+            pagamentos.remove(pagamentoTransacaoId); 
+            System.out.println("Pagamento cancelado para a transação ID: " + pagamentoTransacaoId);
+        } else {
+            System.out.println("Transação ID não encontrada: " + pagamentoTransacaoId);
+        }
+    }
 }
